4033d84701637db2510f9efdfbdc84faad909f96
Eric Caleb <calebme@mail.com>
2017-04-21 12:40:41 +0000

    update makefile output files

--- genesismodule.mod.c
+++ genesismodule.mod.c
@@ -18,6 +18,8 @@ static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
 	{ 0x333ab931, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xa0804566, __VMLINUX_SYMBOL_STR(param_ops_int) },
+	{ 0x46bd19ca, __VMLINUX_SYMBOL_STR(param_ops_charp) },
 	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
 };
